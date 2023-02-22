a1bb7061f691da9bdd95a6fb00c341b932e6134a
ayanle <ayaanlemohamed540@gmail.com>
2023-02-22 18:21:20 +0300

    device_optimizer

--- app/src/main/java/com/ayaanle/device_optimizer/BatteryFragment.java
+++ app/src/main/java/com/ayaanle/device_optimizer/BatteryFragment.java
@@ -29,13 +29,13 @@ public class BatteryFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(BatteryFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
-            }
-        });
+//        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                NavHostFragment.findNavController(BatteryFragment.this)
+//                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+//            }
+//        });
     }
 
     @Override
--- app/src/main/java/com/ayaanle/device_optimizer/Disk.java
+++ app/src/main/java/com/ayaanle/device_optimizer/Disk.java
@@ -1,56 +1,56 @@
-package com.ayaanle.device_optimizer;
-
-import android.app.AlertDialog;
-import android.content.Context;
-import android.os.Build;
-import android.os.Environment;
-import android.os.storage.StorageManager;
-import android.os.storage.StorageVolume;
-import android.widget.Toast;
-
-import androidx.annotation.RequiresApi;
-
-import java.io.File;
-import java.util.List;
-
-@RequiresApi(api = Build.VERSION_CODES.N)
-public class Disk {
-    Context context;
-    StorageManager storage_manager = (StorageManager)context.getSystemService(Context.STORAGE_SERVICE);
-    StorageVolume volume = storage_manager.getPrimaryStorageVolume();
-    @RequiresApi(api = Build.VERSION_CODES.R)
-    public int disk_usage(int size , boolean encrypted)
-    {
-        File _directory = Environment.getDataDirectory();
-        if(storage_manager.isEncrypted())
-            encrypted = true
-        else
-            encrypted = false;
-    }
-    public String disk_info()
-    {
-        String root;
-
-    }
-    public List<String> absolute_paths(List<String>abs_paths , boolean encrypted)
-    {
-
-    }
-    public int getNumberOfPartitions()
-    {
-
-        if(volume.isPrimary() && !volume.isRemovable())
-        {
-            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
-                File directory = volume.getDirectory();
-            }
-        }
-        else if(Environment.isExternalStorageRemovable()){
-            Toast.makeText(context.getApplicationContext(), "the external storage is emulated" , Toast.LENGTH_SHORT).show();
-        }
-    }
-    public AlertDialog DiskDialog()
-    {
-        return new AlertDialog();
-    }
-}
+//package com.ayaanle.device_optimizer;
+//
+//import android.app.AlertDialog;
+//import android.content.Context;
+//import android.os.Build;
+//import android.os.Environment;
+//import android.os.storage.StorageManager;
+//import android.os.storage.StorageVolume;
+//import android.widget.Toast;
+//
+//import androidx.annotation.RequiresApi;
+//
+//import java.io.File;
+//import java.util.List;
+//
+//@RequiresApi(api = Build.VERSION_CODES.N)
+//public class Disk {
+//    Context context;
+//    StorageManager storage_manager = (StorageManager)context.getSystemService(Context.STORAGE_SERVICE);
+//    StorageVolume volume = storage_manager.getPrimaryStorageVolume();
+//    @RequiresApi(api = Build.VERSION_CODES.R)
+//    public int disk_usage(int size , boolean encrypted)
+//    {
+//        File _directory = Environment.getDataDirectory();
+//        if(storage_manager.isEncrypted())
+//            encrypted = true
+//        else
+//            encrypted = false;
+//    }
+//    public String disk_info()
+//    {
+//        String root;
+//
+//    }
+//    public List<String> absolute_paths(List<String>abs_paths , boolean encrypted)
+//    {
+//
+//    }
+//    public int getNumberOfPartitions()
+//    {
+//
+//        if(volume.isPrimary() && !volume.isRemovable())
+//        {
+//            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
+//                File directory = volume.getDirectory();
+//            }
+//        }
+//        else if(Environment.isExternalStorageRemovable()){
+//            Toast.makeText(context.getApplicationContext(), "the external storage is emulated" , Toast.LENGTH_SHORT).show();
+//        }
+//    }
+//    public AlertDialog DiskDialog()
+//    {
+//        return new AlertDialog();
+//    }
+//}
